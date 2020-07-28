68c632ad7327e63da9823973f85a8174a3684f95
stackur <kiliankunkel@gmx.de>
2020-07-28 14:10:12 +0200

    0728_2

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,19 +3,11 @@ import React, { Component } from 'react';
 
 class Square extends React.Component{
     
-    //field
-    field: number;
 
-    //constructor
-    constructor (field: number){
-        super(field);
-        this.field = field;
-    }
-    
     render() {
         return (
           <button className="square">
-            {this.field}
+            {}
           </button>
         );
     }
